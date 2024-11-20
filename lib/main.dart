53a1d5ad0c1cf7e1d9a41df419e9fb6a93f3292d
momenl-ashmawy <momen.lashmawy@gmail.com>
2024-11-20 10:54:52 +0200

    remove get storage add mockito package

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'dart:async';
 
 import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 
 import 'app_module.dart';
 import 'app_widget.dart';
 
 Future<void> main() async {
-  await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('weather');
